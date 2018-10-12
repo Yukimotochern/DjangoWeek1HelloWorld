2f4ac46f54aa573d76fd73c252c06a886b43119d
Yukimotochern <shtomcom@gmail.com>
2018-10-12 14:28:07 +0800

    Database TO postgres

--- helloworld/settings.py
+++ helloworld/settings.py
@@ -76,10 +76,15 @@ WSGI_APPLICATION = 'helloworld.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'kpr',
+        'USER': 'yukimoto',
+        'PASSWORD': 'a130153113',
+        'HOST': '',
+        'PORT': '',
     }
 }
+
 # import dj_database_url
 # DATABASES = {
 #     'default': dj_database_url.config(),
